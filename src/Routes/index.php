694d9a4c779196cc94688165f0e09a6be9704505
Michael Faria <michaelronaldfaria@gmail.com>
2026-01-22 14:21:32 +0000

    Updated routes.

--- src/Routes/index.php
+++ src/Routes/index.php
@@ -3,6 +3,9 @@
 namespace In3050Inm428WebDev\PhpMvc;
 
 use In3050Inm428WebDev\PhpMvc\Controllers\HomeController;
+use In3050Inm428WebDev\PhpMvc\Controllers\ContactController;
+use In3050Inm428WebDev\PhpMvc\Controllers\GalleryController;
+use In3050Inm428WebDev\PhpMvc\Controllers\TestimonialsController;
 use In3050Inm428WebDev\PhpMvc\Router;
 
 $router = new Router();
@@ -10,7 +13,9 @@ $router = new Router();
 $router->get('/', HomeController::class, 'index');
 $router->get('/index.php', HomeController::class, 'index');
 $router->get('/index.html', HomeController::class, 'index');
-//$router->get('/recipes.php', RecipesController::class, 'index');;
+$router->get('/contact_us.html', ContactController::class, 'index');
+$router->get('/gallery.html', GalleryController::class, 'index');
+$router->get('/testimonials.html', TestimonialsController::class, 'index');
 
 $router->dispatch();
 
