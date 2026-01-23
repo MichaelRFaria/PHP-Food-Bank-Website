bf6b927e92a3733eac307aa2647387d55a001e92
Michael Faria <michaelronaldfaria@gmail.com>
2026-01-23 12:25:06 +0000

    Added cookie removal on logout.

--- includes/logout.php
+++ includes/logout.php
@@ -1,8 +1,30 @@
 <?php
+require_once('dbconnect.php');
+
 session_start();
+
+// removing cookies from db
+if (!empty($_COOKIE['userid']) && !empty($_COOKIE['userauth'])) {
+    $userid = $_COOKIE['userid'];
+    $remembered_uid = $_COOKIE['userauth'];
+
+    $connection = db_connect();
+
+    $stmt = $connection->prepare('DELETE FROM remembered_logins WHERE id = ? AND uid = ?');
+    $stmt->bind_param('ss', $userid, $remembered_uid);
+    $stmt->execute();
+
+    $connection->close();
+}
+
+// expire cookies to remove from system
+setcookie('userauth', '', time() - 3600, '/', $_SERVER['SERVER_NAME']);
+setcookie('userid', '', time() - 3600, '/', $_SERVER['SERVER_NAME']);
+setcookie("remember", '', time() - 3600, '/', $_SERVER['SERVER_NAME']);
+
 // clear session
 session_unset();
 session_destroy();
-// Redirect to the login page:
+// Redirect to the index page:
 header('Location: /website/');
 ?>
