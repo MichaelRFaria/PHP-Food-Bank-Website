59986a81d2391d0c36d124988ec0218b3a00807b
Michael Faria <michaelronaldfaria@gmail.com>
2026-01-24 12:15:59 +0000

    Implemented error checking into updating opening times system.

--- includes/update_opening_times.php
+++ includes/update_opening_times.php
@@ -1,19 +1,33 @@
 <?php
 
+session_start();
+
 require_once('./dbconnect.php');
 require_once("./validate_session.php");
 
 // if not volunteer
-if (!isset($SESSION['loggedin']) || $_SESSION['role'] !== "staff") {
-	$_SESSION['error'] = 'You must be logged in as a staff to update opening hours.';
+if (!isset($_SESSION['loggedin']) || $_SESSION['role'] !== "staff") {
+	$_SESSION['error'] = 'You must be logged in as staff to update opening hours.';
     header('Location: /website/opening-times');
+    exit();
 }
 
 $connection = db_connect();
 
+if ($_POST['open_time'] > $_POST['close_time']) {
+    $_SESSION['error'] = 'Opening time cannot be after the closing time.';
+    header('Location: /website/opening-times');
+    exit();
+}
+
 $stmt = $connection->prepare('UPDATE opening_hours SET open_time = ?, close_time = ? WHERE id = ?');
 $stmt->bind_param('ssi', $_POST['open_time'], $_POST['close_time'], $_POST['id']);
-$stmt->execute();
+
+if ( $stmt->execute() ) {
+    $_SESSION['message'] = 'Successfully updated opening times.';
+} else {
+    $_SESSION['error'] = 'Unsuccessful updating of opening times.';
+}
 
 $connection->close();
 
