ecd133b70e37223f539a4b909809ee2a1a028490
Michael Faria <michaelronaldfaria@gmail.com>
2026-01-24 12:06:08 +0000

    Implemented error checking into registration page.

--- includes/create_account.php
+++ includes/create_account.php
@@ -1,7 +1,7 @@
 <?php
 require_once('./dbconnect.php');
-// session_start();
-// unset($_SESSION["error"]);
+session_start();
+unset($_SESSION["error"]);
 
 // // If the user is not logged in redirect to the login page...
 // if (isset($_SESSION['loggedin'])) {
@@ -9,22 +9,30 @@ require_once('./dbconnect.php');
 // 	exit;
 // }
 
-// double check username passwords set.
+// double check all inputs are set.
 if (!isset($_POST['name'],$_POST['phone_number'], $_POST['dob'], $_POST['email'], $_POST['password'], $_POST['confirmation_password'])) {
-	// $_SESSION["error"] = 'Please enter name, phone number, date of birth, email, password and confirmation password.';
-    // header('Location: /website/register');
+	$_SESSION["error"] = 'Please enter name, phone number, date of birth, email, password and confirmation password.';
+    header('Location: /website/register');
+    exit();
 }
 
+// checking password matches confirmation password
 if ($_POST['password'] != $_POST['confirmation_password']) {
-	// $_SESSION["error"] = 'Password and confirmation password must match.';
-    // header('Location: /website/register');
+	$_SESSION["error"] = 'Password and confirmation password must match.';
+    header('Location: /website/register');
+    exit();
 }
 
-// add some date checking logic here
-// if ($_POST['dob'] ) {
-// 	$_SESSION["error"] = 'Must be older than 18 to make an account.';
-//     header('Location: /website/register');
-// }
+// checking user is above the age of 18
+$dob = new DateTime($_POST['dob']);
+$today = new DateTime();
+$minimumDob = $today->sub(new DateInterval('P18Y'));
+
+if ($dob > $minimumDob) {
+	$_SESSION["error"] = 'Must be older than 18 to make an account.';
+    header('Location: /website/register');
+    exit();
+}
 
 // if no errors
 if (empty($_SESSION["error"])) {
@@ -42,8 +50,12 @@ if (empty($_SESSION["error"])) {
         if ($stmt->num_rows > 0) {
             $stmt->bind_result($account_count);
             $stmt->fetch();
-            // if ($account_count > 0)
-            //     $_SESSION["error"] = "The provided email address is already in use.";
+            if ($account_count > 0) {
+                $_SESSION["error"] = "The provided email address is already in use.";
+                    header('Location: /website/register');
+                    exit();
+            }
+        
         }
     }
 
@@ -52,8 +64,9 @@ if (empty($_SESSION["error"])) {
         $hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
         $stmt->bind_param('ssss', $_POST['name'],$_POST['email'],$_POST['phone_number'],$hash);
         $stmt->execute();
-        // $_SESSION["message"] = "Your account has been created.";
+        $_SESSION["message"] = "Your account has been created.";
         header('Location: /website/login');
+        exit();
     }
 
     // Close connection
