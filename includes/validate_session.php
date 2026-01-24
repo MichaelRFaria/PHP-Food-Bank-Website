5fd0331ed790989c5ec0bf622d065ecb6b91fdb8
Michael Faria <michaelronaldfaria@gmail.com>
2026-01-24 12:16:20 +0000

    Implemented error checking into session validation system.

--- includes/validate_session.php
+++ includes/validate_session.php
@@ -9,16 +9,19 @@ $timeout = ($_SESSION["role"] == "staff") ? 28800 : 3600;
 
 // if not logged in, log in
 if (!isset($_SESSION["loggedin"])) {
+    $_SESSION["error"] = "Please log in to access this content.";
     header("Location: /website/login");
+    exit();
 }
 
 // if session is expired, re log in
-
 if (!isset($_SESSION['latest_action_timestamp']) && ((time() - $_SESSION['latest_action_timestamp']) > $timeout)) {
     session_unset();
     session_destroy();
 
+    $_SESSION['error'] = 'Session expired, please log in again.';
     header("Location: /website/login");
+    exit();
 }
 
 // otherwise update timetamp
